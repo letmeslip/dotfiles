1fadb46f2f8f58b298f5e261c67bb8b2bf73a5ac
ひょろ <t.yuma0306@gmail.com>
2026-04-10 07:09:51 +0900

    Add zsh configs and update setup script with Oh My Zsh/plugins install

--- setup.sh
+++ setup.sh
@@ -4,16 +4,66 @@ DOTFILES_DIR="$HOME/dotfiles"
 
 # dotfilesをclone（まだなければ）
 if [ ! -d "$DOTFILES_DIR" ]; then
-    git clone git@github.com:letmeslip/dotfiles.git "$DOTFILES_DIR"
+    git clone https://github.com/letmeslip/dotfiles.git "$DOTFILES_DIR"
 fi
 
-# .tmux.conf のシンボリックリンクを張る
-if [ -f "$HOME/.tmux.conf" ] && [ ! -L "$HOME/.tmux.conf" ]; then
-    mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak"
-    echo "既存の .tmux.conf を .tmux.conf.bak にバックアップしました"
+# OS判定
+if [[ "$(uname)" == "Darwin" ]]; then
+    OS="mac"
+else
+    OS="linux"
 fi
 
-ln -sf "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
-echo ".tmux.conf のシンボリックリンクを作成しました"
+# シンボリックリンクを張る関数
+link() {
+    local src="$DOTFILES_DIR/$1"
+    local dst="$HOME/$1"
+    if [ -f "$dst" ] && [ ! -L "$dst" ]; then
+        mv "$dst" "$dst.bak"
+        echo "既存の $1 を $1.bak にバックアップしました"
+    fi
+    ln -sf "$src" "$dst"
+    echo "$1 のシンボリックリンクを作成しました"
+}
 
-echo "完了！tmux を起動して確認してください"
+# Oh My Zshのインストール
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zsh をインストール中..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# zsh-autosuggestionsのインストール
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    echo "zsh-autosuggestions をインストール中..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+fi
+
+# zsh-syntax-highlightingのインストール
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    echo "zsh-syntax-highlighting をインストール中..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+fi
+
+# Powerlevel10kのインストール
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+    echo "Powerlevel10k をインストール中..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+fi
+
+# tmuxのインストール
+if ! command -v tmux &>/dev/null; then
+    echo "tmux をインストール中..."
+    if [[ "$OS" == "mac" ]]; then
+        brew install tmux
+    else
+        sudo apt install -y tmux 2>/dev/null || sudo dnf install -y tmux 2>/dev/null
+    fi
+fi
+
+# シンボリックリンクを張る
+link .tmux.conf
+link .zshrc
+link .p10k.zsh
+
+echo ""
+echo "完了！ source ~/.zshrc で設定を反映してください"
